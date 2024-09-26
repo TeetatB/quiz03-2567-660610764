cf92a0144d8c02d3acb00ecdf0bb53692bbd7f44
TeetatB <teetatmix36108@gmail.com>
2024-09-26 15:09:13 +0700

    almost done

--- src/app/api/messages/route.ts
+++ src/app/api/messages/route.ts
@@ -37,46 +37,73 @@ export const GET = async (request: NextRequest) => {
 
 export const POST = async (request: NextRequest) => {
   readDB();
+  const body = await request.json();
+  const {roomId,messageText} = body;
+  const checkRoomId = (<Database>DB).rooms.find(
+    (x)=>(x.roomId === roomId)
+  );
 
-  // return NextResponse.json(
-  //   {
-  //     ok: false,
-  //     message: `Room is not found`,
-  //   },
-  //   { status: 404 }
-  // );
+  if(!checkRoomId){
+    return NextResponse.json(
+      {
+        ok: false,
+        message: `Room is not found`,
+      },
+      { status: 404 }
+    );
+  }
 
-  const messageId = nanoid();
+   const messageId = nanoid();
+  //push message into the room with id that we assign
+  (<Database>DB).messages.push(
+    roomId,
+    messageId,
+    messageText
+  );
 
   writeDB();
 
   return NextResponse.json({
     ok: true,
-    // messageId,
+    messageId: messageId,
     message: "Message has been sent",
   });
 };
 
 export const DELETE = async (request: NextRequest) => {
   const payload = checkToken();
+  const body = await request.json();
+  const {roomId, messageText} = body;
 
-  // return NextResponse.json(
-  //   {
-  //     ok: false,
-  //     message: "Invalid token",
-  //   },
-  //   { status: 401 }
-  // );
+  if(!payload){
+    return NextResponse.json(
+      {
+        ok: false,
+        message: "Invalid token",
+      },
+      { status: 401 }
+    );
+  }
+  
 
   readDB();
-
-  // return NextResponse.json(
-  //   {
-  //     ok: false,
-  //     message: "Message is not found",
-  //   },
-  //   { status: 404 }
-  // );
+  const messageId = nanoid();
+  const checkmessage = (<Database>DB).messages.find(
+    (x)=>(x.messageId === messageId && x.messageText === messageText && x.roomId === roomId) 
+  );
+  if(!checkmessage){
+    return NextResponse.json(
+      {
+        ok: false,
+        message: "Message is not found",
+      },
+      { status: 404 }
+    );
+  }
+  
+  (<Database>DB).messages.filter(
+    (x)=>(x.messageId === messageId && x.messageText === messageText && x.roomId === roomId)
+  );
 
   writeDB();
 
